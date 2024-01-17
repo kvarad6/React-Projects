bffb7eebfa9ab4e6dc9586bbd6eedda1f4d8f813
varad.kulkarni <kvarad6@gmail.com>
2024-01-17 15:10:23 +0530

    updated routes in app.js

--- firebase-login/src/App.js
+++ firebase-login/src/App.js
@@ -4,13 +4,14 @@ import { Routes, Route } from 'react-router-dom';
 import Signup from './Components/auth/Signup';
 import Signin from './Components/auth/Signin'
 import AuthDetails from './Components/AuthDetails';
-
+import Home from './Components/Home';
 
 function App() {
   return (
     <>
       <Box>
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="signup" element={<Signup />} />
           <Route path="signin" element={<Signin />} /> 
         </Routes>
